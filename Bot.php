6b02cf1bed86e06071a67299922afa5ac6ddc468
tanawine <31724534+tanawine@users.noreply.github.com>
2017-09-15 18:21:13 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -33,28 +33,6 @@
 				$post = json_encode($data);
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
-			}
-			else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && strpos( $textUser,"ตัว") ) { //&& $event['message']['text'] == "ไร"
-				// Get text sent
-				$text = "เป็นส่วนหนึ่งของ  ";
-				
-				// Get replyToken
-				$replyToken = $event['replyToken'];
-				
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];
-				// Make a POST Request to Messaging API to reply to sender
-				$url = 'https://api.line.me/v2/bot/message/reply';
-				$data = [
-					'replyToken' => $replyToken,
-					'messages' => [$messages],
-				];
-				$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-				$ch = curl_init($url);
 			}else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "สวัสดี") {
 				// Get text sent
 				$text = "สวัสดีจ้า ";
@@ -87,5 +65,5 @@
 				echo $result . "\r\n";
 		}
 	}
-	echo "OK5";
-	//echo "OK9999";
+	//echo "OK5";
+	echo "OK9999";
