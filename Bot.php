b5073ef431d99ff5b53004b9f30116645ee76a1f
tanawine <31724534+tanawine@users.noreply.github.com>
2017-09-15 18:47:40 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -74,7 +74,7 @@
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
 			}
-			else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && strpos( $textUser,"To day homeWork?") ) { //&& $event['message']['text'] == "ไร"
+			else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && strpos( $textUser,"homeWork") ) { //&& $event['message']['text'] == "ไร"
 			// Get text sent
 			$text = "Subjects have 1. ";
 			
