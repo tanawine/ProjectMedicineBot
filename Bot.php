3f754cfdabcc6b1aded05d28c45f66946e61e7dc
tanawine <31724534+tanawine@users.noreply.github.com>
2017-09-29 14:52:29 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -102,27 +102,7 @@
 				$post = json_encode($data);
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
-			 }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "สวัสดี") {
-				// Get text sent
-				$text = "สวัสดีจ้า ";
-				// Get replyToken
-				$replyToken = $event['replyToken'];
-		
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];
-				// Make a POST Request to Messaging API to reply to sender
-				$url = 'https://api.line.me/v2/bot/message/reply';
-				$data = [
-					'replyToken' => $replyToken,
-					'messages' => [$messages],
-				];
-				$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-				$ch = curl_init($url);
-			}else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "ชื่ออะไร") {
+			 }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "ชื่ออะไร") {
 				// Get text sent
 				$text = "ชื่อ Medicine Bot จ้า ";
 				// Get replyToken
