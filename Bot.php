0e23b8549ff5ca3242f868945bffa15dcd882ac9
tanawine <31724534+tanawine@users.noreply.github.com>
2017-10-30 19:44:14 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -181,33 +181,7 @@
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
 			 }
-			else if (strpos( $textUser,"ผื่น") ||  $event['message']['text'] == "ผื่น"
-				|| strpos( $textUser,"คัน") ||  $event['message']['text'] == "คัน" 
-				|| strpos( $textUser,"ตุ่มน้ำ") ||  $event['message']['text'] == "ตุ่มน้ำ"
-				||strpos( $textUser,"ตุ่มแดง") ||  $event['message']['text'] == "ตุ่มแดง "
-				||strpos( $textUser,"จุดแดง") ||  $event['message']['text'] != "จุดแดง"
-				||strpos( $textUser,"ตุ่มใส") ||  $event['message']['text'] != "ตุ่มใส") { 
-
-				$text = "เป็นอะไรไหนบอกหมอได้นะครับ บอกทีละอาการจะได้ให้คำแนะนำได้ง่ายๆ ";
-				
-				// Get replyToken
-				$replyToken = $event['replyToken'];
-				
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];
-				// Make a POST Request to Messaging API to reply to sender
-				$url = 'https://api.line.me/v2/bot/message/reply';
-				$data = [
-					'replyToken' => $replyToken,
-					'messages' => [$messages],
-				];
-				$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-				$ch = curl_init($url);
-			 }
+			
 
 			
 				//$ch = curl_init($url);
@@ -221,5 +195,5 @@
 				echo $result . "\r\n";
 		}
 	}
-	//echo "OK555";
-	echo "OK9997";
+	echo "OK555";
+	//echo "OK9997";
