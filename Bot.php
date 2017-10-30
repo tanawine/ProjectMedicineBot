91bc7d2f14a4913347244c251238620fdc65437b
tanawine <31724534+tanawine@users.noreply.github.com>
2017-10-30 18:59:18 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -12,7 +12,7 @@
 			// Reply only when message sent is in 'text' format
 			 $textUser = $event['message']['text'];
 			 //$text1 = "ไร";
-		 if (strpos( $textUser,"จ้า") ||  $event['message']['text'] == "จ้า" ) { //&& $event['message']['text'] == "ไร"
+		 if (strpos( $textUser,"จ้า") ||  $event['message']['text'] == "จ้า" ) { 
 				// Get text sent
 
 				$text = "วันนี้สบายดีไหมครับ";
@@ -36,7 +36,10 @@
 				$ch = curl_init($url);
 			
 			}
-			else if (strpos( $textUser,"ไม่ป่วย") ||  $event['message']['text'] == "ไม่ป่วย" || strpos( $textUser,"สบายดี") ||  $event['message']['text'] == "สบายดี" || strpos( $textUser,"ไม่เป็นอะไร") ||  $event['message']['text'] == "ไม่เป็นอะไร"||strpos( $textUser,"ไม่เป็นไร") ||  $event['message']['text'] == "ไม่เป็นไร") { //&& $event['message']['text'] == "ไร"
+			else if (strpos( $textUser,"ไม่ป่วย") ||  $event['message']['text'] == "ไม่ป่วย" 
+				 || strpos( $textUser,"สบายดี") ||  $event['message']['text'] == "สบายดี" 
+				 || strpos( $textUser,"ไม่เป็นอะไร") ||  $event['message']['text'] == "ไม่เป็นอะไร"
+				 ||strpos( $textUser,"ไม่เป็นไร") ||  $event['message']['text'] == "ไม่เป็นไร") { 
 				// Get text sent
 
 				$text = "ดีแล้วครับ ดูแลตัวเองอย่าให้ป่วยนะครับ ";
@@ -59,7 +62,11 @@
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
 			 }
-			else if (strpos( $textUser,"ไอ") ||  $event['message']['text'] == "ไอ" || strpos( $textUser,"ไอเจ็บคอ") ||  $event['message']['text'] == "ไอเจ็บคอ" || strpos( $textUser,"ไอค่ะ") ||  $event['message']['text'] == "ไอค่ะ" || strpos( $textUser,"ไอครับ") ||  $event['message']['text'] == "ไอครับ"||strpos( $textUser,"เจ็บคอ") ||  $event['message']['text'] == "เจ็บคอ") { //&& $event['message']['text'] == "ไร"
+			else if (strpos( $textUser,"ไอ") ||  $event['message']['text'] == "ไอ" || 
+				 strpos( $textUser,"ไอเจ็บคอ") ||  $event['message']['text'] == "ไอเจ็บคอ" || 
+				 strpos( $textUser,"ไอค่ะ") ||  $event['message']['text'] == "ไอค่ะ" || 
+				 strpos( $textUser,"ไอครับ") ||  $event['message']['text'] == "ไอครับ"||
+				 strpos( $textUser,"เจ็บคอ") ||  $event['message']['text'] == "เจ็บคอ") 
 				// Get text sent
 
 				$text = "\t\tถ้ามีอาการไอแบบมีเสมหะ หมอแนะนำว่า ให้ใช้ตัวยาที่มีส่วนประกอบของ Guaifenesin ซึ่งช่วยบรรเทาอาการไอชนิดมีเสมหะ\tโดยยาที่หาซื้อได้ทั่วไปคือ Bromhexine หรือ Bisolvon \n***กรณี อยากเห็นภาพตัวอย่างยา พิมพ์คำว่า Bromhexine หรือ Bisolvon \n\n กรณี มีอาการไอแบบไม่มีเสมหะ พิมพ์คำว่า ไอแบบไม่มีเสมหะ";
@@ -84,10 +91,24 @@
 				
 				
 			}
-			else if (strpos( $textUser,"เมื่อย") ||  $event['message']['text'] == "เมื่อย"||strpos( $textUser,"ปวดแขน") ||  $event['message']['text'] == "ปวดแขน"||strpos( $textUser,"ปวดขา") ||  $event['message']['text'] == "ปวดขา"||strpos( $textUser,"ปวดเข่า") ||  $event['message']['text'] == "ปวดเข่า"||strpos( $textUser,"ปวดไหล่") ||  $event['message']['text'] == "ปวดไหล่"||strpos( $textUser,"ปวดหลัง
-			") ||  $event['message']['text'] == "ปวดหลัง"||strpos( $textUser,"ปวดตัว") ||  $event['message']['text'] == "ปวดตัว"||strpos( $textUser,"ปวดต้นขา") ||  $event['message']['text'] == "ปวดต้นขา") { 
+			else if (strpos( $textUser,"เมื่อย") ||  $event['message']['text'] == "เมื่อย"||
+				 strpos( $textUser,"ปวดแขน") ||  $event['message']['text'] != "ปวดแขน"||
+				 strpos( $textUser,"ปวดขา") ||  $event['message']['text'] != "ปวดขา"||
+				 strpos( $textUser,"ปวดเข่า") ||  $event['message']['text'] != "ปวดเข่า"||
+				 strpos( $textUser,"ปวดไหล่") ||  $event['message']['text'] != "ปวดไหล่"||
+				 strpos( $textUser,"ปวดหลัง") ||  $event['message']['text'] != "ปวดหลัง"||
+				 strpos( $textUser,"ปวดตัว") ||  $event['message']['text'] != "ปวดตัว"||
+				 strpos( $textUser,"ปวดต้นขา") ||  $event['message']['text'] != "ปวดต้นขา"||
+				strpos( $textUser,"ปวดข้อเท้า") ||  $event['message']['text'] != "ปวดข้อเท้า"||
+				strpos( $textUser,"ปวดข้อมือ") ||  $event['message']['text'] != "ปวดข้อมือ"||
+				strpos( $textUser,"ปวดเอว") ||  $event['message']['text'] != "ปวดเอว"||
+				strpos( $textUser,"ปวดน่อง") ||  $event['message']['text'] != "ปวดน่อง"||
+				strpos( $textUser,"เคล็ดขัดยอก") ||  $event['message']['text'] != "เคล็ดขัดยอก") { 
 
-				$text = "\tกรณีที่ปวดเมื่อยกล้ามเนื้อจากการทำกิจกรรมในชีวิตประจำวัน ที่เป็นอาการปวดเมื่อยเบื้องต้น หมอแนะนำให้ใช้แผ่นแปะบรรเทาปวด หรือใช้ยาแก้ปวดเมื่อยชนิดนวด\n*กรณีอยากเห็นรูปตัวอย่างยาให้พิมพ์คำว่า plaster หรือ ยาแก้ปวดกล้ามเนื้อ  \n**อยากทราบวิธีดูแลอาการปวดเมื่อย พิมพ์  ปวดTC\n***อยากทราบ ความแตกต่างของยาแก้ปวดเมื่อยชนิดนวด แบบร้อน และ แบบเย็น  พิมพ์  แตกต่างยาแก้ปวดกล้ามเนื้อ";
+				$text = "\tกรณีที่ปวดเมื่อยกล้ามเนื้อจากการทำกิจกรรมในชีวิตประจำวัน ที่เป็นอาการปวดเมื่อยเบื้องต้น 
+				หมอแนะนำให้ใช้แผ่นแปะบรรเทาปวด หรือใช้ยาแก้ปวดเมื่อยชนิดนวด\n*กรณีอยากเห็นรูปตัวอย่างยาให้พิมพ์คำว่า plaster 
+				หรือ ยาแก้ปวดกล้ามเนื้อ  \n**อยากทราบวิธีดูแลอาการปวดเมื่อย พิมพ์  ปวดTC\n***อยากทราบ 
+				ความแตกต่างของยาแก้ปวดเมื่อยชนิดนวด แบบร้อน และ แบบเย็น  พิมพ์  แตกต่างยาแก้ปวดกล้ามเนื้อ";
 				
 				// Get replyToken
 				$replyToken = $event['replyToken'];
@@ -107,7 +128,8 @@
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
 			 }
-			else if (strpos( $textUser,"ไข้") ||  $event['message']['text'] == "ไข้" || strpos( $textUser,"มีไข้") ||  $event['message']['text'] == "มีไข้" ) {
+			else if (strpos( $textUser,"ไข้") ||  $event['message']['text'] == "ไข้" || 
+				 strpos( $textUser,"มีไข้") ||  $event['message']['text'] == "มีไข้" ) {
 				// Get text sent
 
 				$text = "อาการของคนมีไข้ คือ อุณหภูมิกายเพิ่มสูงกว่าปกติ คือ 36 หรือ 37.2 องศา หากวัดจากทางปาก เวลาที่มีไข้ไม่จำเป็นว่าทุกส่วนของร่างกายจะต้องร้อนเท่ากันหมดนะครับ อาจร้อนที่ศีรษะ ลำตัว และแขนขา แต่ฝ่ามือฝ่าเท้ายังเย็นเป็นปกติก็ได้นะครับ โดยยาที่ใช้ลดไข้คือ Paracetamol โดยไข้หวัดธรรมดาจะมีอาการ 3-5 วัน ถ้ามีอาการมากกว่านี้แนะนำให้พบแพทย์นะครับ \n*กรณีอยากเห็นรูปตัวอย่างยาให้พิมพ์คำว่า paracetamol \n**กรณีอยากทราบวิธีดูตัวเองพิมพ์ ไข้TC";
@@ -171,5 +193,5 @@
 				echo $result . "\r\n";
 		}
 	}
-	//echo "OK555";
-	echo "OK9997";
+	echo "OK555";
+	//echo "OK9997";
