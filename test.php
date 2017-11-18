e38de9b6c5e0d5112fa2566817869a41c6a6c426
tanawine <31724534+tanawine@users.noreply.github.com>
2017-11-18 19:03:59 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Tanawine 555";
+$dbname = 'User';
+$dbuser = 'root';
+$dbpass = '';
+$dbhost = 'localhost';
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+
+echo "Win" ;
